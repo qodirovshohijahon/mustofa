9e4e92c65431e8040f16910e73fe4c6e3474d561
Shokhijakhon Kodirov <45763633+qodirovshohijahon@users.noreply.github.com>
2022-03-27 23:49:13 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Be With Mustofa
         </a>
       </header>
     </div>
